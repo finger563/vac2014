ace37a825612d2abbcba38baf6cd8054c4a2da64
William Emfinger <waemfinger@gmail.com>
2014-02-21 10:48:27 -0600

    Working implementation of ground station using OF.  Receives image in thread which stores into an ofImage buffer; main thread writes items in this buffer to the screen and to a file.

---
+++ ground_station/groundStation/src/testApp.cpp
@@ -0,0 +1,149 @@
+#include "testApp.h"
+
+#define RECONNECT_TIME 400
+#define IMG_BUFFER_SIZE 10
+#define MAXBUFLEN 10240
+
+const char* IP_ADDR = "10.1.1.2";
+const int IP_PORT = 9999;
+
+//--------------------------------------------------------------
+DWORD WINAPI recvImageFunction( LPVOID lpParam ) {
+	testApp* app;
+	app = (testApp *)lpParam;
+	
+	char tmpBuffer[50];
+
+	sprintf(tmpBuffer,"Start Processing");
+	app->udpConnection.Send(tmpBuffer,strlen(tmpBuffer));
+	printf("Ground Station: Sent Initialization\n");
+	app->udpConnection.Receive(tmpBuffer,50);
+	printf("Ground Station: Got Response = %s\n",tmpBuffer);
+
+	while (true) {
+		app->receiveImage();
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::receiveImage() {
+	unsigned char *data;
+	char tmpBuffer[MAXBUFLEN];
+	memset(tmpBuffer,0,MAXBUFLEN);
+	int imgSize = 0;
+	int recvBytes = 0;
+	bool recvEnd = false;
+	int totalBytesReceived = 0;
+
+	bool recvStart = false;
+	while ( !recvStart ) {
+		while ( (recvBytes = udpConnection.Receive(tmpBuffer,MAXBUFLEN) ) <= 0 );
+		char *p = strtok(tmpBuffer,",");
+		if ( p != NULL && !strcmp(p,"START") ){
+			recvStart = true;
+			p = strtok(NULL,","); // want second argument
+			imgSize = atoi(p);
+			data = new unsigned char[imgSize];
+			printf("Ground Station: image size = %d\n",imgSize);
+			if ( imgSize <= 0 ) {
+				recvStart = false;
+			}
+			else {
+				while (totalBytesReceived < imgSize && !recvEnd) {
+					recvBytes = udpConnection.Receive((char *)data + totalBytesReceived,MAXBUFLEN);
+					totalBytesReceived += recvBytes;
+					if (!strcmp(tmpBuffer,"END")) {
+						printf("Ground Station: image ended with %s\n",tmpBuffer);
+						recvEnd = true;
+					}
+				}
+			}
+		}
+	}
+	for (int i=0;i<imgSize;i++)
+		iBuffer.getWriter()->getPixels()[i] = data[i];
+	delete data;
+	iBuffer.incrementWriter();
+	printf("Ground Station: Received %d bytes\n",totalBytesReceived);
+}
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	iBuffer.allocate(IMG_BUFFER_SIZE,this->width,this->height);
+	
+	udpConnection.Create();
+	udpConnection.Connect(IP_ADDR, IP_PORT);
+
+	receiverThread = CreateThread( NULL, 0, recvImageFunction, this, 0, &dwThreadID);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	if ( !iBuffer.isEmpty() )
+		iBuffer.getReader()->update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	static int imgNum = 0;
+	char fname[50];
+	if ( !iBuffer.isEmpty() )
+		iBuffer.getReader()->draw(0,0);
+	//ofDrawBitmapString("VAC Ground Station Monitor", 15, 30);
+	if ( iBuffer.numImages() > 1 ) {
+		sprintf(fname,"img%05d.png",imgNum);
+		iBuffer.getReader()->saveImage(fname);
+		iBuffer.incrementReader();
+		imgNum++;
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+	string message="";
+	udpConnection.Send(message.c_str(),message.length());
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ ground_station/groundStation/src/testApp.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "ofxNetwork.h"
+#include "ofMain.h"
+
+#include <Windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+class imgBuffer {
+ 
+ public:
+
+  imgBuffer() {
+    buffer = NULL;
+    read_ptr = -1;
+    write_ptr = -1;
+    buffer_size = -1;
+    num_images = 0;
+  }
+
+  ~imgBuffer() {
+    if (buffer != NULL){
+      delete[] buffer;
+    }
+  }
+
+  int allocate(int size, int w, int h) {
+    try {
+      buffer = new ofImage[size];
+	  for (int i=0;i<size;i++) {
+		  buffer[i].allocate(w,h,OF_IMAGE_COLOR);
+	  }
+    }
+    catch ( ... ) {
+      printf("Error: couldn't allocate image buffer of size %d\n",size);
+      if (buffer != NULL)
+		delete[] buffer;
+      return -1;
+    }
+    buffer_size = size;
+    read_ptr = 0;
+    write_ptr = 0;
+    num_images = 0;
+    return 0;
+  }
+
+  ofImage* getReader() { 
+	  return &buffer[read_ptr]; 
+  }
+
+  ofImage* getWriter() {
+	  return &buffer[write_ptr];
+  }
+
+  void incrementWriter() {
+    write_ptr++;
+    if ( write_ptr >= buffer_size ) {
+      write_ptr = 0;
+    }
+    num_images++;
+  }
+  void incrementReader() {
+    read_ptr++;
+    if ( read_ptr >= buffer_size ) {
+      read_ptr = 0;
+    }
+    num_images--;
+  }
+
+  bool isEmpty() { return num_images <= 0; }
+
+  bool isFull() { return num_images >= buffer_size; }
+
+  int numImages() { return num_images; }
+  
+ private:
+  int num_images;
+  int buffer_size;
+  int read_ptr,write_ptr;
+  ofImage* buffer;
+};
+
+DWORD WINAPI recvImageFunction( LPVOID lpParam );
+
+class testApp : public ofBaseApp{
+
+	public:
+		testApp(int w, int h){
+			width = w; height = h;
+		}
+
+		void setup();
+		void update();
+		void draw();
+		void exit();
+		
+		void receiveImage();
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);		
+
+		ofxUDPManager udpConnection;
+
+		int width,height;
+		imgBuffer iBuffer;
+
+		HANDLE receiverThread;
+		DWORD dwThreadID;
+
+		ofTrueTypeFont  mono;
+		ofTrueTypeFont  monosm;
+		//vector<ofPoint> stroke;
+};
+
